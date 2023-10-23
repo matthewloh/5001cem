59f5229ed2088a13e05ebe109c3bfeb15304454a
matthewloh <64674205+matthewloh@users.noreply.github.com>
2023-10-24 01:31:20 +0800

    added patient healthrecord creation and updated db schema

--- views/registrationForms/patientForm.py
+++ views/registrationForms/patientForm.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 from datetime import datetime
+from ttkbootstrap.scrolled import ScrolledText
+from ttkbootstrap.dialogs import Messagebox, MessageDialog, Querybox
 from resource.basewindow import ElementCreator, gridGenerator
 from resource.static import *
 from tkinter import *
@@ -25,7 +27,11 @@ class PatientRegistrationForm(Frame):
         self.createElements()
 
     def createFrames(self):
-        pass
+        self.inputframe = self.controller.frameCreator(
+            x=40, y=100, framewidth=720, frameheight=780,
+            bg=WHITE, classname="inputframe", root=self
+        )
+        self.inputframe.grid_remove()
 
     def createElements(self):
         self.controller.labelCreator(
@@ -34,6 +40,7 @@ class PatientRegistrationForm(Frame):
         )
         self.createLabels()
         self.createButtons()
+        self.initializeFormVars()
 
     def createLabels(self):
         self.roleImg = self.controller.labelCreator(
@@ -42,13 +49,14 @@ class PatientRegistrationForm(Frame):
         )
 
     def createButtons(self):
-        OPT1STR = "Existing Health Records"
-        OPT2STR = "Current Medications"
-        OPT3STR = "Allergies"
-        OPT4STR = "Family History"
-        OPT5STR = "Additional Biodata"
-        OPT6STR = "Other Information"
-        optList = [OPT1STR, OPT2STR, OPT3STR, OPT4STR, OPT5STR, OPT6STR]
+        self.OPT1STR = "Existing Health Records"
+        self.OPT2STR = "Current Medications"
+        self.OPT3STR = "Allergies"
+        self.OPT4STR = "Family History"
+        self.OPT5STR = "Additional Biodata(eg. Past Surgeries)"
+        self.OPT6STR = "Input Height, Weight and Blood Type"
+        optList = [self.OPT1STR, self.OPT2STR, self.OPT3STR,
+                   self.OPT4STR, self.OPT5STR, self.OPT6STR]
         # creating a button arrangement of two per row
         # iterates over the list of options and creates a button for each
         for i, option in enumerate(optList):
@@ -58,7 +66,7 @@ class PatientRegistrationForm(Frame):
                 ipath="assets/Registration/Patient/PatientButtonOptionBg.png",
                 x=x, y=y, classname="formoption" + str(i + 1), root=self,
                 text=option, size=20, font=INTER,
-                buttonFunction=lambda o=option: self.loadSpecificSubmission(o)
+                buttonFunction=lambda o=option: self.loadInputForOption(o)
             )
         self.completeRegBtn = self.controller.buttonCreator(
             ipath="assets/Registration/CompleteRegButton.png",
@@ -66,8 +74,75 @@ class PatientRegistrationForm(Frame):
             buttonFunction=lambda: self.confirmSubmission()
         )
 
-    def loadSpecificSubmission(self, option: str):
-        print(option)
+    def initializeFormVars(self):
+        self.vars = {
+            self.OPT1STR: StringVar(),
+            self.OPT2STR: StringVar(),
+            self.OPT3STR: StringVar(),
+            self.OPT4STR: StringVar(),
+            self.OPT5STR: StringVar(),
+        }
+
+    def loadInputForOption(self, option):
+        if option == self.OPT6STR:
+            # Ask user to input height and weight
+            self.weight = Querybox.get_float(
+                title="Input Weight", prompt="Input your weight in kg",
+                initialvalue=0, minvalue=0, maxvalue=200, parent=self.controller.widgetsDict["formoption6"]
+            )
+            self.height = Querybox.get_float(
+                title="Input Height", prompt="Input your height in cm",
+                initialvalue=0, minvalue=0, maxvalue=300, parent=self.controller.widgetsDict["formoption6"]
+            )
+            # Ask user to input blood type
+            self.bloodType = Querybox.get_string(
+                title="Input Blood Type", prompt="Input your blood type",
+                initialvalue="O+", parent=self.controller.widgetsDict["formoption6"]
+            )
+            return
+        self.loadInfoLabel(option)
+        self.createEditText()
+        if self.vars[option].get() != "":
+            self.inputTextSpace.insert("1.0", self.vars[option].get())
+        self.configSaveClearButtons(option)
+
+    def configSaveClearButtons(self, option):
+        BUTTONCREATOR = self.controller.buttonCreator
+
+        BUTTONCREATOR(
+            ipath="assets/Registration/Patient/Back.png", x=640, y=10,
+            classname="reg_backbutton", root=self.inputframe,
+            buttonFunction=lambda: [self.inputframe.grid_remove()],
+            isPlaced=True
+        )
+        BUTTONCREATOR(
+            ipath="assets/Registration/Patient/ClearText.png", x=100, y=680,
+            classname="reg_cleartextbutton", root=self.inputframe,
+            buttonFunction=lambda: [self.inputTextSpace.delete("1.0", END)],
+        )
+        BUTTONCREATOR(
+            ipath="assets/Registration/Patient/SaveText.png", x=400, y=680,
+            classname="reg_savetext", root=self.inputframe,
+            buttonFunction=lambda: [
+                self.vars[option].set(self.inputTextSpace.get("1.0", "end-1c"))],
+        )
+
+    def createEditText(self):
+        self.inputText = ScrolledText(
+            master=self.inputframe, width=520, height=520, padding=0,
+            bootstyle="success-round", autohide=True)
+        self.inputText.place(x=100, y=140, width=520, height=520)
+        self.inputTextSpace = self.inputText.text
+        self.inputTextSpace.config(font=("Inter", 20), fg=BLACK)
+
+    def loadInfoLabel(self, option):
+        self.inputframe.grid()
+        self.inputframe.tkraise()
+        infolabel = self.controller.textElement(
+            ipath="assets/Registration/InputFormTextBG.png", x=0, y=0,
+            classname="inputformtext", root=self.inputframe, text=f"Input your {option} here",
+            size=30, font=INTER
+        )
 
     def confirmSubmission(self):
         prisma = self.prisma
@@ -104,14 +179,14 @@ class PatientRegistrationForm(Frame):
                 },
                 "healthRecord": {
                     "create": {
-                        "allergies": "test",
-                        "bloodType": "test",
-                        "height": 0,
-                        "weight": 0,
-                        "currentMedication": "test",
-                        "pastMedication": "test",
-                        "pastSurgery": "test",
-                        "familyHistory": "test",
+                        "allergies": self.vars[self.OPT3STR].get(),
+                        "bloodType": self.bloodType,
+                        "height": float(self.height),
+                        "weight": float(self.weight),
+                        "currentMedication": self.vars[self.OPT2STR].get(),
+                        "pastMedication": self.vars[self.OPT1STR].get(),
+                        "pastSurgery": self.vars[self.OPT5STR].get(),
+                        "familyHistory": self.vars[self.OPT4STR].get(),
                     }
                 }
             }
