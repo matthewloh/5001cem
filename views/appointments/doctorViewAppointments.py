9e5b2a45b2345935a53ccf6dace3811b0546d71c
matthewloh <64674205+matthewloh@users.noreply.github.com>
2023-10-24 15:25:26 +0800

    scrolled frame example in doctorviewappointments

--- views/appointments/doctorViewAppointments.py
+++ views/appointments/doctorViewAppointments.py
@@ -26,7 +26,7 @@ class DoctorViewAppointments(Frame):
         self.parent = parent
         gridGenerator(self, 84, 54, "#dee8e0")
         self.grid(row=0, column=12, columnspan=84, rowspan=54, sticky=NSEW)
-        
+
         self.prisma = self.controller.mainPrisma
         self.createFrames()
         self.createElements()
@@ -36,6 +36,43 @@ class DoctorViewAppointments(Frame):
 
     def createElements(self):
         self.controller.labelCreator(
-            ipath="assets/Dashboard/DoctorAssets/DoctorRequestPrescription.png",
+            ipath="assets/Appointments/Doctor/BG.png",
             x=0, y=0, classname="appointmentspanelbg", root=self
         )
+        self.loadAppScrolledFrame()
+
+    def loadAppScrolledFrame(self):
+        prisma = self.prisma
+        # appointments = prisma.appointment.find_many(
+        #     where={
+        #         "doctor": {
+        #             "is": {
+        #                 "userId": self.getUserID()
+        #             }
+        #         }
+        #     },
+        #     include={
+        #         "patient": {
+        #             "include": {
+        #                 "user": True
+        #             }
+        #         }
+        #     }
+        # )
+        appointments = [1, 2, 3, 4, 5, 6, 7]
+        h = len(appointments) * 120
+        if h < 620:
+            h = 620
+        self.appointmentScrolledFrame = ScrolledFrame(
+            master=self, width=1500, height=h, bootstyle="bg-round", autohide=True
+        )
+        self.appointmentScrolledFrame.place(
+            x=80, y=320, width=1500, height=620)
+        initCoords = (20, 20)
+        for a in appointments:
+            bg = self.controller.labelCreator(
+                ipath="assets/Appointments/Doctor/SingleAppBG.png",
+                x=initCoords[0], y=initCoords[1], classname=f"appointmentbg{a}", root=self.appointmentScrolledFrame,
+                isPlaced=True
+            )
+            initCoords = (initCoords[0], initCoords[1] + 120)
