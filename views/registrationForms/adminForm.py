bdbf596af1792a34674fac0b5925f79a25588f66
mch3pg4 <mch3pg4@gmail.com>
2023-10-26 19:18:54 +0800

    updated clinic admin form

--- views/registrationForms/adminForm.py
+++ views/registrationForms/adminForm.py
@@ -110,6 +110,7 @@ class AdminRegistrationForm(Frame):
         self.clinicstateVar = StringVar()
         self.cliniczipVar = StringVar()
         self.cliniccontactnumberVar = StringVar()
+        self.clinichrsVar = StringVar()
 
     def loadSpecificSubmission(self, option: str):
         if option == self.OPT1STR:
@@ -215,6 +216,15 @@ class AdminRegistrationForm(Frame):
                 R: frame,
                 PH: "Clinic Zip"
             },
+            "clinichrs": {
+                X: 40,
+                Y: 600,
+                W: 300,
+                H: 80,
+                CN: "clinichrsentry",
+                R: frame,
+                PH: "Clinic Hours"
+            },
         }
         for p in param:
             CREATOR(**param[p])
@@ -234,8 +244,10 @@ class AdminRegistrationForm(Frame):
         WD["cliniccityentry"].insert(0, self.cliniccityVar.get())
         WD["clinicstateentry"].insert(0, self.clinicstateVar.get())
         WD["cliniczipentry"].insert(0, self.cliniczipVar.get())
+        WD["clinichrsentry"].insert(0, self.clinichrsVar.get())
 
     def saveClinicInformation(self):
+        prisma = self.prisma
         WD = self.controller.widgetsDict
         self.clinicnameVar.set(
             WD["clinicnameentry"].get())
@@ -249,6 +261,8 @@ class AdminRegistrationForm(Frame):
             WD["clinicstateentry"].get())
         self.cliniczipVar.set(
             WD["cliniczipentry"].get())
+        self.clinichrsVar.set(
+            WD["clinichrsentry"].get())
         msg = f"""
         Clinic Information Saved!
         Name: {self.clinicnameVar.get()}
@@ -257,12 +271,16 @@ class AdminRegistrationForm(Frame):
         City: {self.cliniccityVar.get()}
         State: {self.clinicstateVar.get()}
         Zip: {self.cliniczipVar.get()} 
+        Hours: {self.clinichrsVar.get()}
         """
         Messagebox.show_info(
             title="Clinic Information",
             message=msg,
             parent=self.inputframe
         )
+
+
+        
 
     def loadCloseAndSaveButtons(self):
         self.closebutton.grid()
@@ -466,7 +484,21 @@ class AdminRegistrationForm(Frame):
                         "zip": self.cliniczipVar.get(),
                         "clinicImg": self.getBase64Data(),
                         "phoneNum": self.cliniccontactnumberVar.get(),
+                        "clinicHrs": self.clinichrsVar.get(),
                     }
                 }
             }
         )
+
+        # clinicEnrolment = prisma.clinicenrolment.create(
+        #     data={
+        #         "clinic": {
+        #             "connect": {
+        #                 "id": clinicAdmin.id
+        #             }
+        #         },
+        #         "createdAt": datetime.now(tz=timezone("Asia/Kuala_Lumpur")).strftime("%Y-%m-%d %H:%M:%S"),
+        #         "updatedAt": datetime.now(tz=timezone("Asia/Kuala_Lumpur")).strftime("%Y-%m-%d %H:%M:%S"),
+        #         "status": "PENDING"
+        #     }
+        # )
